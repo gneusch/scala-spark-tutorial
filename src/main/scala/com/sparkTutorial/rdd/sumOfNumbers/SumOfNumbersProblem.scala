d4ec7a7468d31b9cced8922388ee94320d2c28c6
answeris42 <gneusch@gmail.com>
2019-03-04 21:29:02 +0100

    my sumofnumbers solution

--- src/main/scala/com/sparkTutorial/rdd/sumOfNumbers/SumOfNumbersProblem.scala
+++ src/main/scala/com/sparkTutorial/rdd/sumOfNumbers/SumOfNumbersProblem.scala
@@ -1,5 +1,10 @@
 package com.sparkTutorial.rdd.sumOfNumbers
 
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.{SparkConf, SparkContext}
+
+import scala.util.Try
+
 object SumOfNumbersProblem {
 
   def main(args: Array[String]) {
@@ -9,5 +14,21 @@ object SumOfNumbersProblem {
 
        Each row of the input file contains 10 prime numbers separated by spaces.
      */
+    Logger.getLogger("org").setLevel(Level.ERROR)
+    val conf = new SparkConf().setAppName("sumOfPrimes").setMaster("local")
+    val sc = new SparkContext(conf)
+
+    val lines = sc.textFile("in/prime_nums.text")
+    val numbers = lines.flatMap(line => line.split("\t"))
+      .map(num => {
+        Try(Integer.parseInt(num.trim)).toOption.getOrElse(0)
+      })
+
+    val numberList = numbers.collect
+    numberList.foreach(number => println(s"$number "))
+
+    val sum = numbers.reduce((x, y) => x + y)
+    println(sum)
+
   }
 }
